ab8144f759cdfbd75378fb4b3b844ac7160e41b0
Brittanie Campos <bri2212040@maricopa.edu>
2022-06-23 08:53:21 -0700

    Add project files.

---
+++ ForEachLoop/Program.cs
@@ -0,0 +1,10 @@
+﻿// foreach loop = a simpler way to iterate over an array, but it's less flexible
+
+String[] cars = { "BMW", "Mustang", "Corvette" };
+
+foreach(String car in cars)
+{
+    Console.WriteLine(car);
+}
+
+Console.ReadKey();
